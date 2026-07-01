6ba8c800f6e794e6e1917b2ae6e8bb74c3e04a71
rodri_frodi <rodripiano@gmail.com>
2026-07-01 11:47:16 +0200

    Actualizado JSON armas melee.

--- src/main/java/ArmaMelee.java
+++ src/main/java/ArmaMelee.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArmaMelee extends Contenedor {
@@ -5,21 +6,23 @@ public class ArmaMelee extends Contenedor {
     private String tipoArma;
     private int dano;
     private int modificadorHA;
+    private int ignoraTA;
     private int velocidad;
     private int entereza;
     private int rotura;
     private String fuerzaRequerida;
     private String criticoPrimario;
     private String criticoSecundario;
-    private String especializaciones;
+    private List<String> especializaciones;
 
     public ArmaMelee(){}
 
-    public ArmaMelee(String nombre, int presenciaBase, List<String> especialidades, String tipoArma, int dano, int modificadorHA, int velocidad, int entereza, int rotura, String fuerzaRequerida, String criticoPrimario, String criticoSecundario, String especializaciones){
+    public ArmaMelee(String nombre, int presenciaBase, List<String> especialidades, String tipoArma, int dano, int modificadorHA, int ignoraTA, int velocidad, int entereza, int rotura, String fuerzaRequerida, String criticoPrimario, String criticoSecundario, List<String> especializaciones){
         super(nombre, presenciaBase, especialidades);
         this. tipoArma = tipoArma;
         this.dano = dano;
         this.modificadorHA = modificadorHA;
+        this.ignoraTA = ignoraTA;
         this.velocidad = velocidad;
         this.entereza = entereza;
         this.rotura = rotura;
@@ -36,6 +39,7 @@ public class ArmaMelee extends Contenedor {
         return dano;
     }
     public int getModificadorHA() { return modificadorHA; }
+    public int getIgnoraTA() { return ignoraTA; }
     public int getVelocidad(){
         return velocidad;
     }
@@ -52,5 +56,5 @@ public class ArmaMelee extends Contenedor {
         return criticoPrimario;
     }
     public String getCriticoSecundario(){ return criticoSecundario; }
-    public String getEspecializaciones(){ return especializaciones; }
+    public List<String> getEspecializaciones(){ return new ArrayList<>(especializaciones); }
 }
